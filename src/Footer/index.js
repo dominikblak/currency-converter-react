45096cc590acf8c3ee4df9f0d8f04cd8f00dfeaa
Dominik Blak <bldominikak@gmial.com>
2021-06-18 13:37:28 +0200

    footer container

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,10 +1,9 @@
 import "./style.css";
-import "../Container/style.css";
 
 const Footer = (props) => (
   <footer className="footer">
     <div className="footer__container">
-      <a className="footer footer--link" target="blank" href="https://github.com/dominikblak">
+      <a className=" footer__container footer__container--link" target="blank" href="https://github.com/dominikblak">
         Dominik Blak 2021
       </a>
     </div>
