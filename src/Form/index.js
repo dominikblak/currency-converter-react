9ae5cf59fb888ba1d28fffa1fef5801019aa4c26
Dominik Blak <bldominikak@gmial.com>
2021-06-18 14:05:57 +0200

    delete js class

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,43 +1,35 @@
 import "./style.css";
 const Form = () => (
-  <form className="container__form">
-    <fieldset className="container_fieldset">
+  <form className="form">
+    <fieldset className="form__fieldset">
       <legend>Twoje dane</legend>
       <p>
         <label for="amountid">
-          <span className="container__label"> Twoja kwota w zł :</span>
-          <input
-            className="container__input js-input"
-            type="number"
-            name="amount"
-            min="0"
-            step="any"
-            id="amountid"
-            required
-          />
+          <span className="form__label"> Twoja kwota w zł :</span>
+          <input className="form__input" type="number" name="amount" min="0" step="any" id="amountid" required />
         </label>
       </p>
       <p>
         <label for="currency">
-          <span className="container__label"> Wybierz walutę :</span>
-          <select name="currency_choice" id="currency" className="container__input js-container">
-            <option value="EUR" className="container__euro" selected>
+          <span className="form__label"> Wybierz walutę :</span>
+          <select name="currency_choice" id="currency" className="form__input">
+            <option value="EUR" className="form__euro" selected>
               EUR
             </option>
-            <option value="USD" className="container__usd">
+            <option value="USD" className="form__usd">
               USD
             </option>
-            <option value="GBP" className="container__gbp">
+            <option value="GBP" className="form__gbp">
               GBP
             </option>
           </select>
         </label>
       </p>
       <p>
-        <button className="container__btn">Przelicz</button>
+        <button className="form__button">Przelicz</button>
       </p>
       <p>
-        Otrzymasz : <strong className="container__value js-value"></strong>
+        Otrzymasz : <strong className="form__value "></strong>
       </p>
     </fieldset>
   </form>
