f2b785ae30882a1f748de1006a140611f8519b18
Dominik Blak <bldominikak@gmial.com>
2021-06-18 14:53:28 +0200

    array currency

---
+++ src/currency.js
@@ -0,0 +1,6 @@
+const currency = [
+  { currency: "Euro", shortName: "EUR", value: "4.55" },
+  { currency: "Dolar", shortName: "USD", value: "3.83" },
+  { currency: "Funt Brytjski", shortName: "GBP", value: "5.31" },
+];
+export default currency;
