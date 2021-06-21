59178c0660964cf0c50b370e7a966acbc12994f5
Dominik Blak <bldominikak@gmial.com>
2021-06-21 20:37:43 +0200

    gird in result

--- src/Result/index.js
+++ src/Result/index.js
@@ -3,8 +3,9 @@ export const Result = ({ result }) => (
   <p className="result">
     {result !== undefined && (
       <>
-        <strong>
-          {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+        Otrzymasz : &nbsp;
+        <strong class="result result--yourcurrency">{result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;</strong>
+        <strong class="result result--selectedcurrency">
           {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
         </strong>
       </>
