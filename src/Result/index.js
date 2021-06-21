4cd9d46cdbcf64dac66a087847f9917718fc1ca4
Dominik Blak <bldominikak@gmial.com>
2021-06-21 19:58:27 +0200

    react fragment

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,14 +1,13 @@
 import "./style.css";
 export const Result = ({ result }) => (
-  <p>
-    {result !== undefined &&
-      `
-    <>
-Otrzymasz : <strong className="result">
-${result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
-${result.targetAmount.toFixed(2)}&nbsp;${result.currency}
-     </strong>
-     </>
-            `}
+  <p className="result">
+    {result !== undefined && (
+      <>
+        <strong>
+          {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+          {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+        </strong>
+      </>
+    )}
   </p>
 );
