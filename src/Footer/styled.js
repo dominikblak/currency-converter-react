96f24d941ab27fa7d6e2fdbcb436b1b5aab301c6
Dominik Blak <bldominikak@gmial.com>
2021-07-16 13:50:33 +0200

    styled component footer

---
+++ src/Footer/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterContent = styled.footer`
+  text-align: center;
+`;
+export const FooterContainer = styled(FooterContent)`
+  padding: 20px;
+`;
+export const FooterContainerLink = styled(FooterContent)`
+  text-decoration: none;
+  color: white;
+`;
