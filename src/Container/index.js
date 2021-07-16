98c396bc753f87492062177dbca2e5bc6264a03d
Dominik Blak <bldominikak@gmial.com>
2021-07-16 09:59:44 +0200

    styled component Container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,3 +1,4 @@
 import "./style.css";
-const Container = (props) => <section className="container">{props.children}</section>;
+import { Content } from "./styled";
+const Container = (props) => <Content>{props.children}</Content>;
 export default Container;
---
+++ src/Container/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Content = styled.section`
+  max-width: 1000px;
+  margin: 0 auto;
+  color: white;
+  font-family: "Titillium Web", sans-serif;
+  padding: 20px;
+`;
