52a259c06df81955bf5a6693bb9793f20a50425f
Dominik Blak <bldominikak@gmial.com>
2021-07-27 17:52:15 +0200

    theme in Result & Header components

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -4,7 +4,7 @@ export const Content = styled.h1`
   margin-top: 100px;
   text-align: center;
   font-size: 3.5em;
-  @media (max-width: 767px) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     margin-top: 50px;
   }
 `;
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,9 @@
 export const theme = {
   color: {
-    white: "FFFFFF",
+    white: "#FFFFFF",
     gradient: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))",
+    green: "#008000",
+    red: "#FF0000",
   },
   breakpoint: {
     mobileMax: 767,
