41f43e6f71f9508fa3aae405b771681a3f988929
Dominik Blak <bldominikak@gmial.com>
2021-07-27 18:17:42 +0200

    globalStyle

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,8 @@ export const theme = {
     gradient: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))",
     green: "#008000",
     red: "#FF0000",
+    blue: "#0000FF",
+    mintgreen: "#99FF99",
   },
   breakpoint: {
     mobileMax: 767,
