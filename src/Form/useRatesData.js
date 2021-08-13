61dc7f10d79f5c7a32dc0fe0a4b9057d6aa46d54
Dominik Blak <bldominikak@gmial.com>
2021-08-13 16:15:44 +0200

    setTimeOut in useRatesData

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 
-export useRatesData = ()=>{
-    const [ratesData, setRatesData] = useState({
-        state : "loading",
-    });
-    useEffect(()=>{
-        cosnt fetchRates = async()=>{
-            try{
-                const response = await fetch("https://api.exchangerate.host/convert?from=USD&to=EUR");
+export const useRatesData = () => {
+  const [ratesData, setRatesData] = useState({
+    state: "loading",
+  });
+  useEffect(() => {
+    const fetchRates = async () => {
+      try {
+        const response = await fetch("https://api.exchangerate.host/convert?from=USD&to=EUR");
 
-                if(!response.ok){
-                    throw new Error (response.statusText);
-                }
-                const {ratesData, date} = await response.json();
-
-                setRatesData({
-                    state:"success",
-                    rates,
-                    date,
-                });
-            }
+        if (!response.ok) {
+          throw new Error(response.statusText);
         }
-    })
-}
+        const { rates, date } = await response.json();
+
+        setRatesData({
+          state: "success",
+          rates,
+          date,
+        });
+      } catch {
+        setRatesData({
+          state: "error",
+        });
+      }
+    };
+    setTimeout(fetchRates, 1000);
+  }, []);
+  return ratesData;
+};
