e682526bdda90d514b16f17777a33cfae4b0f1dd
Dominik Blak <bldominikak@gmial.com>
2021-08-18 18:42:40 +0200

    center form

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -11,15 +11,28 @@ export const Wrapper = styled.fieldset`
 export const Span = styled.span`
   display: inline-block;
   width: 150px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    display: block;
+    text-align: center;
+    margin: 10px auto;
+  }
 `;
 export const Input = styled.input`
   margin-top: 10px;
   margin-left: 30px;
   width: 150px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    display: block;
+    text-align: center;
+    margin: 20px auto;
+  }
 `;
 export const Button = styled.button`
   display: block;
   margin: 30px auto 10px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin-top: 40px;
+  }
 `;
 export const Loading = styled.p`
   color: ${({ theme }) => theme.color.green};
